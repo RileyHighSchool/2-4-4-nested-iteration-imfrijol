eac4005cd792e64ef5e8382aa1084609164131ef
imfrijol <milton87863@students.sbcsc.k12.in.us>
2023-01-13 13:38:49 -0500

    final LetterFrequencies

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -11,5 +11,16 @@ public class LetterFrequencies
 		System.out.println("The following shows the letter frequencies for the phrase");
 
     /* your code here */
+	for (int i= 0; i < 26; i++){
+	String let = letters.substring(i, i+1);
+	int count = 0;
+		for (int j = 0; j < phrase.length(); j++){
+			String lett= phrase.substring(j,j+1);
+			if(let.equals(lett)){
+				count = count + 1;
+			}
+		  }
+		  System.out.println("There is " + count + " " + let + " in this phrase.");
+		}
 	}
 }
